3c9bd018cc1935fe7c3927aac56db0c7c04b8708
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-09 21:49:31 -0300

    aula java

---
+++ aula.java
@@ -0,0 +1,21 @@
+package aula;
+
+public class aula {
+    public static void main(String[] args) {
+
+
+    String saudacao = "Hello World!";
+    System.out.println(saudacao.toLowerCase());
+    System.out.println(saudacao.toUpperCase());
+    System.out.println(saudacao.indexOf("o"));
+    System.out.println(saudacao.charAt(6));
+
+    //aula 4
+    String aluno_1 = "miranata";
+    String aluno_2 = "aeronauta";
+    System.out.println(aluno_1.equals(aluno_2));
+    String mensagem = "Hello World!";
+    System.out.println("-" + mensagem.trim() + "-");
+
+    }
+}
