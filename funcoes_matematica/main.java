6f42c102fbc08cdbaec9166c8c90b5a078be7f6b
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-09 21:36:17 -0300

    main java

--- funcoes_matematica/main.java
+++ funcoes_matematica/main.java
@@ -1,5 +1,12 @@
 package funcoes_matematica;
 
 public class main {
-    
+    public static void main (String[] args) {
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt((double)64.0F));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.pow(2,8));
+        System.out.println(Math.round(4.6));
+    }
 }
